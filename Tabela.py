82e749816208f5819c96a740fb37c8aeda0cc6c5
Giovanna Pessoa <gipessoa12@gmail.com>
2024-05-15 18:05:16 -0300

    Tabela Periodica

---
+++ Tabela.py
@@ -0,0 +1,10 @@
+elemento = input ("digite o elemento")
+elemento = elemento.lower()
+if elemento == 'o':
+    print("Oxigenio, 8")
+elif elemento == 'n':
+    print("nitrogenio,7")
+elif elemento == 'c':
+    print("carbono,6")
+elif elemento == 's':
+    print("enxofre,16")
